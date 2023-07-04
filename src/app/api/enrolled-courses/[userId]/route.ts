7ecb88f900cff8e9c5bb6be428afa9d58663f30e
Kuo Yu-Tsung <erickuo5124@gmail.com>
2023-07-04 12:02:25 +0800

    feat(Dashboard): get enrolled course list

---
+++ src/app/api/enrolled-courses/[userId]/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import { PrismaClient } from '@prisma/client'
+
+import { unserialize } from "@/wp-utils/unserializeData";
+import { formatTime } from '@/utils'
+
+const prisma = new PrismaClient()
+
+export async function GET(
+  req: Request,
+  { params }: { params: { userId: string } }
+) {
+  const courseIDs = await prisma.wp_posts.findMany({
+    where: {
+      post_type: 'tutor_enrolled',
+      post_status: 'completed',
+      post_author: Number(params.userId)
+    },
+    select: {
+      post_parent: true,
+    }
+  })
+  return NextResponse.json(courseIDs.map((course: any) => {
+    return { id: Number(course.post_parent) }
+  }))
+}
--- src/app/dashboard/EnrollCourses.tsx
+++ src/app/dashboard/EnrollCourses.tsx
@@ -1,15 +1,51 @@
 'use client'
 
-import { Col, Divider, Text } from "@nextui-org/react"
+import { useState, useEffect } from 'react'
+import { Col, Divider, Row, Text } from "@nextui-org/react"
+import { useSession } from 'next-auth/react';
+import { customFetch } from "@/utils/customFetch"
+import { useAppContext } from "@/appContext";
 
-type Props = {}
 
-const EnrollCourses = (props: Props) => {
+const getEnrolledList = async (userId: string | undefined) => {
+  if (!userId) return []
+  const url = `/api/enrolled-courses/${userId}`;
+  const res = await customFetch(url);
+  return res;
+};
+
+const EnrollCourses = () => {
+  const [enrolledId, setEnrolledId] = useState<string[]>([]);
+  const { data: session } = useSession();
+  const userId = session?.user?.id;
+  const courses = useAppContext();
+  useEffect(() => {
+    const getEnrolled = async () => {
+      const enrolledList = await getEnrolledList(userId);
+      setEnrolledId(enrolledList);
+    }
+    getEnrolled();
+  }, [userId])
   return (
     <Col>
       <Text h3>我的課程</Text>
       <Divider />
-      {/* get enrolled courses */}
+      <Row>
+        {courses
+          .filter((course: CourseListType) => {
+            const isEnrolled = enrolledId.find((enrolledCourse: any) =>
+              enrolledCourse.id === course.id
+            )
+            return isEnrolled
+          })
+          .map((course: CourseListType) => {
+            return (
+              <Col key={course.id}>
+                <Text h4>{course.title}</Text>
+              </Col>
+            )
+          })}
+      </Row>
     </Col>
   )
 }
